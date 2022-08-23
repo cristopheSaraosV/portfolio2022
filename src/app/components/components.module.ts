b1727aa5dd9fd4c5fe3bb648602b53b0bad6f34b
cristopher <crisotopher.saraos@gmail.com>
2022-08-22 20:58:06 -0400

    Finish

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { AsideComponent } from './aside/aside.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 
 @NgModule({
-  declarations: [MenuComponent, AsideComponent],
+  declarations: [MenuComponent, AsideComponent, PresentationComponent, ContactComponent],
   imports: [
     CommonModule
   ],
   exports:[
     MenuComponent,
-    AsideComponent
+    AsideComponent,
+    PresentationComponent,
+    ContactComponent
   ]
 })
 export class ComponentsModule { }
