2a8156308271d6f6af9a0a167ae862f868684c2e
cristopher <crisotopher.saraos@gmail.com>
2022-08-22 22:20:18 -0400

    Aside finished

--- src/app/components/presentation/presentation.component.ts
+++ src/app/components/presentation/presentation.component.ts
@@ -9,7 +9,49 @@ export class PresentationComponent implements OnInit {
 
   constructor() { }
 
+  courses = [
+    {
+      "course":"INTRODUCTION TO WEB DEVELOPMENT 1 ( 40 h )",
+      "url":"www.google.cl"
+    },
+    {
+      "course":"INTRODUCTION TO WEB DEVELOPMENT 2 ( 40 h )",
+      "url":"www.google.cl"
+    },
+    {
+      "course":"PERSONAL PRODUCTIVITY ( 40 h )",
+      "url":"www.google.cl"
+    },
+    {
+      "course":"SCRUM FOUNDATION PROFESSIONAL CERTIFICATE",
+      "url":"www.google.cl"
+    },
+    {
+      "course":"PROGRAMMING WITH JAVASCRIPT ED 5",
+      "url":"www.google.cl"
+    },
+    {
+      "course":"ANGULAR: FROM ZERO TO EXPERT - ( EDITION 2021 )",
+      "url":"www.google.cl"
+    },
+    {
+      "course":"ADVANCED ANGULAR: TAKE YOUR BASES TO THE NEXT LEVEL - MEAN",
+      "url":"www.google.cl"
+    },
+  ]
+  courseSelected = 0;
+
   ngOnInit(): void {
+  }
+
+  changeCourse(position:number){
+    if(this.courseSelected == 0 && position == -1){
+      this.courseSelected = this.courses.length-1;
+    }else if(this.courseSelected>=0 &&  this.courseSelected+1 < this.courses.length) {
+      this.courseSelected= this.courseSelected + position;
+    }else{
+      this.courseSelected = 0;
+    }
   }
 
 }
